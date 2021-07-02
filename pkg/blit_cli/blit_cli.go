a7643a2186e87855af6c932cbf80b4cc68db6fe4
Ruymán Borges R <ruyman21@gmail.com>
2021-07-02 22:20:08 +0100

    Modified blit_cli.go for error hanlding

--- pkg/blit_cli/blit_cli.go
+++ pkg/blit_cli/blit_cli.go
@@ -103,6 +103,7 @@ func EncapData(fileInfo []fs.FileInfo, path string) ([][]string, error, int64) {
 	for _, file := range fileInfo {
 		fName := file.Name()
 		fmt.Println("File name: ", fName)
+		fmt.Println("Path: ", path)
 		fmt.Println("Path + file name: ", path + fName)
 		stats, err := os.Stat(path + fName)
 		//stats, err := os.Stat(fName)
